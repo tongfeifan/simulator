977a40a56834ee210aabfbb469c9f7dcf4c6615b
Feifan <tongfeifan@gmail.com>
2015-03-15 02:02:53 +0000

    add the GE data

---
+++ Trader.py
@@ -0,0 +1,7 @@
+class trader(object):
+	"""docstring for trader"""
+	def __init__(self, ID strategy_ID):
+		self.ID = ID
+		self.strategy_ID = strategy_ID
+	def operateTrede(self.strategy_ID):
+		pass
